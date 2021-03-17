c4d63c796d98e58ea6c64361d6d7ada740d4bfa1
angeloplacebo <public.angelo@gmail.com>
2021-03-17 17:04:58 -0300

    previous-messages implemented

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('views', path.join(__dirname, 'public'))
 
 app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
 app.use('/', (req, res) => {
   res.render('index.html')
@@ -18,16 +20,20 @@ app.use('/', (req, res) => {
 server.listen(3000)
 
 const users = {}
+let messages = []
 
 io.on('connection', socket => {
-  console.log('New User')
+  console.log(`New user connected: ${socket.id}`)
+  
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
+    socket.emit('previous-messages',messages)
   })
 
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id]})
+    messages.push({author: users[socket.id], message: message, })
+    socket.broadcast.emit('chat-message', { message: message, author: users[socket.id]})
   })
 
   socket.on('disconnect', ()=>{
@@ -36,6 +42,3 @@ io.on('connection', socket => {
   })
 
 })
-
-
-
